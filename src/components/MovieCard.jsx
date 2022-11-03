ef821491739fca241c210e46110c2f95224bbb37
Cooper <dancersoftaranto@gmail.com>
2022-11-03 15:40:10 +0300

    clean

--- src/components/MovieCard.jsx
+++
@@ -1,7 +0,0 @@
-const MovieCard = () => {
-  return (
-    <div>MovieCard</div>
-  )
-}
-
-export default MovieCard
--- src/pages/Rgister.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Rgister = () => {
-  return (
-    <div>Rgister</div>
-  )
-}
-
-export default Rgister
