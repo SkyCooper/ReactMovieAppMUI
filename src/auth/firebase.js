24b70974b67766edb350f7d3fcc56383229e082b
Cooper <dancersoftaranto@gmail.com>
2022-11-03 18:19:55 +0300

    firebase

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,9 +1,100 @@
-import React from 'react'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
-const firebase = () => {
-  return (
-    <div>firebase</div>
-  )
-}
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC47GkduSZdmOTlh0XXr57W6ZMG2ZcJk6I",
+  authDomain: "movie-app-e48e8.firebaseapp.com",
+  projectId: "movie-app-e48e8",
+  storageBucket: "movie-app-e48e8.appspot.com",
+  messagingSenderId: "760674641161",
+  appId: "1:760674641161:web:131f3f4dc0b54f63cf5fd8",
 
-export default firebase
+  // apiKey: process.env.REACT_APP_apiKey,
+  // authDomain: process.env.REACT_APP_authDomain,
+  // projectId: process.env.REACT_APP_projectId,
+  // storageBucket: process.env.REACT_APP_storageBucket,
+  // messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  // appId: process.env.REACT_APP_appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export const createUser = async (email, password, navigate, displayName) => {
+  try {
+    let userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    await updateProfile(auth.currentUser, {
+      displayName: displayName,
+    });
+    navigate("/");
+    console.log(userCredential);
+  } catch (error) {
+    console.log(error.message);
+    alert(error.message && "Email already in use!");
+  }
+};
+
+export const signIn = async (email, password, navigate) => {
+  try {
+    let userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    navigate("/");
+    // sessionStorage.setItem('user', JSON.stringify(userCredential.user));
+    console.log(userCredential);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const userObserver = (setCurrentUser) => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setCurrentUser(user);
+    } else {
+      setCurrentUser(false);
+    }
+  });
+};
+
+export const logOut = () => {
+  signOut(auth);
+};
+
+//* => Authentication => sign-in-method => enable Google
+//! Google ile girişi enable yap
+//* => Authentication => sign-in-method => Authorized domains => add domain
+//! Projeyi deploy ettikten sonra google sign-in çalışması için domain listesine deploy linkini ekle
+
+export const signUpProvider = (navigate) => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result);
+      navigate("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
